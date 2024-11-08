8f345f714f5c64d6d80ea765ce01f804c45ce5b1
Tuan Vu <tuanvuf4@gmail.com>
2024-11-09 01:45:09 +0700

    refactor

--- dist/assets/register-CNVhcmhx.js
+++
@@ -1,1 +0,0 @@
-import{g as a,j as s,f as t,aw as r}from"./index-DjsLknDp.js";const l=()=>{const e=a();return s.jsx("div",{className:e.container,children:s.jsx("div",{className:t(e.bodyContent),children:s.jsx(r,{})})})};export{l as default};

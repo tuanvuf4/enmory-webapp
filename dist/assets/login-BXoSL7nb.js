5cda51473828b13d2ff060e2ef79a8d3597f1dd1
Tuan Vu <tuanvuf4@gmail.com>
2025-02-15 00:06:01 +0700

    build

---
+++ dist/assets/login-BXoSL7nb.js
@@ -0,0 +1,1 @@
+import{g as o,j as s,k as e,ax as n}from"./index-DyXFAV45.js";const l=()=>{const a=o();return s.jsx("div",{className:a.container,children:s.jsx("div",{className:e(a.bodyContent),children:s.jsx(n,{})})})};export{l as default};

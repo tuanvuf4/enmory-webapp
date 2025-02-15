03d18e2c54007abe523314c380bdf2eb4dcf8c51
Tuan Vu <tuanvuf4@gmail.com>
2025-02-15 10:11:38 +0700

    build

--- dist/assets/register-DINUvRxD.js
+++
@@ -1,1 +0,0 @@
-import{g as a,j as s,k as t,az as r}from"./index-DyXFAV45.js";const l=()=>{const e=a();return s.jsx("div",{className:e.container,children:s.jsx("div",{className:t(e.bodyContent),children:s.jsx(r,{})})})};export{l as default};

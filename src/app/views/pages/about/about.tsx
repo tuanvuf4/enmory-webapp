aa4113caf6bd7f9a1863a66e02b7326f568d5ff9
Tuan Vu <tuanvuf4@gmail.com>
2025-03-12 21:49:45 +0700

    build

--- src/app/views/pages/about/about.tsx
+++ src/app/views/pages/about/about.tsx
@@ -1,8 +1,8 @@
-import { Col, Row, Space, theme } from 'antd'
+import { Col, Flex, Row, Space, theme } from 'antd'
 import classNames from 'clsx'
 import globalStyle from '@/style/appStyle'
 import { Link } from 'react-router-dom'
-
+import Style from './style'
 import registerStyle from '../register/style'
 import loginStyle from '../login/style'
 import { useAppSelector } from '@/app/core/hooks/redux'
@@ -11,6 +11,7 @@ const About = () => {
   const { token } = theme.useToken()
 
   const gClasses = globalStyle()
+  const style = Style()
   const classesLogin = loginStyle()
   const classesRegister = registerStyle()
 
@@ -21,44 +22,44 @@ const About = () => {
       <h2 className={classNames(gClasses.pageTitle)}>About</h2>
 
       <div className={classNames(gClasses.contentPage)}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac nulla volutpat,
-          auctor mauris vel, lobortis mi. Quisque a felis viverra, facilisis purus vitae, cursus
-          enim. Integer non lacinia arcu. Aenean sollicitudin porta purus ac iaculis. Maecenas
-          congue tincidunt arcu vitae pulvinar. Praesent faucibus pellentesque mi suscipit luctus.
-          Ut rhoncus lacus tellus. Proin quis erat posuere diam volutpat sodales. Donec sit amet
-          nibh urna. Sed nec nisl tincidunt, mollis arcu non, blandit turpis. Nunc fringilla, arcu a
-          pellentesque iaculis, nisl ipsum aliquet massa, volutpat volutpat tortor est ac nibh.
-          Nullam ac rutrum justo. Phasellus eget fermentum urna. Vivamus at commodo elit. In quis
-          sapien eu urna pretium congue ac eu augue. Phasellus mattis, sapien sed imperdiet
-          bibendum, lacus erat efficitur elit, in bibendum sapien augue luctus neque.
-        </p>
+        <Row className={style.item} gutter={[token.size / 2, token.size / 2]}>
+          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 6 }}>
+            <h2>8:00 am - 10:00 am (2 hours)</h2>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac nulla volutpat,
-          auctor mauris vel, lobortis mi. Quisque a felis viverra, facilisis purus vitae, cursus
-          enim. Integer non lacinia arcu. Aenean sollicitudin porta purus ac iaculis. Maecenas
-          congue tincidunt arcu vitae pulvinar. Praesent faucibus pellentesque mi suscipit luctus.
-          Ut rhoncus lacus tellus. Proin quis erat posuere diam volutpat sodales. Donec sit amet
-          nibh urna. Sed nec nisl tincidunt, mollis arcu non, blandit turpis. Nunc fringilla, arcu a
-          pellentesque iaculis, nisl ipsum aliquet massa, volutpat volutpat tortor est ac nibh.
-          Nullam ac rutrum justo. Phasellus eget fermentum urna. Vivamus at commodo elit. In quis
-          sapien eu urna pretium congue ac eu augue. Phasellus mattis, sapien sed imperdiet
-          bibendum, lacus erat efficitur elit, in bibendum sapien augue luctus neque.
-        </p>
+            <ul>
+              <li>Review and learn new words</li>
+              <li>Speak with Elsa app</li>
+            </ul>
+          </Col>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac nulla volutpat,
-          auctor mauris vel, lobortis mi. Quisque a felis viverra, facilisis purus vitae, cursus
-          enim. Integer non lacinia arcu. Aenean sollicitudin porta purus ac iaculis. Maecenas
-          congue tincidunt arcu vitae pulvinar. Praesent faucibus pellentesque mi suscipit luctus.
-          Ut rhoncus lacus tellus. Proin quis erat posuere diam volutpat sodales. Donec sit amet
-          nibh urna. Sed nec nisl tincidunt, mollis arcu non, blandit turpis. Nunc fringilla, arcu a
-          pellentesque iaculis, nisl ipsum aliquet massa, volutpat volutpat tortor est ac nibh.
-          Nullam ac rutrum justo. Phasellus eget fermentum urna. Vivamus at commodo elit. In quis
-          sapien eu urna pretium congue ac eu augue. Phasellus mattis, sapien sed imperdiet
-          bibendum, lacus erat efficitur elit, in bibendum sapien augue luctus neque.
-        </p>
+          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 6 }}>
+            <h2>10:30 am - 12:30 am (2 hours)</h2>
+
+            <ul>
+              <li>Listening (dictation / taking notes)</li>
+              <li>Watching videos (listening + imitating)</li>
+            </ul>
+          </Col>
+
+          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 6 }}>
+            <h2>3:00 pm - 5:00 pm (2 hours)</h2>
+
+            <ul>
+              <li>Reading an article (aloud)</li>
+              <li>Review words</li>
+            </ul>
+          </Col>
+
+          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 6 }}>
+            <h2>8:00 pm - 12:00 pm (4 hours)</h2>
+
+            <ul>
+              <li>Review words</li>
+              <li>Speak with Elsa app</li>
+              <li>Listen to podcast/stories/YouTube (imitate)</li>
+            </ul>
+          </Col>
+        </Row>
 
         {!isAuth && (
           <Space direction='vertical' size={[token.size, token.size]} className={gClasses.fulWidth}>

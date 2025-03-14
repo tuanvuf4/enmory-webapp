129cbea2facbc7781f6f394ed015d5c037c73536
Tuan Vu <tuanvuf4@gmail.com>
2025-03-14 20:35:42 +0700

    build

--- dist/assets/register-DnR-JPtp.js
+++
@@ -1,1 +0,0 @@
-import{g as t,j as s,k as a,bQ as r}from"./index-JP4N0Laa.js";const l=()=>{const e=t();return s.jsx("div",{className:e.container,children:s.jsx("div",{className:a(e.bodyContent),children:s.jsx(r,{})})})};export{l as default};

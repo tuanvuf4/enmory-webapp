0123cb2e8073a839da8ff59534e7d9b9f52a44b9
Tuan Vu <tuanvuf4@gmail.com>
2026-01-17 10:51:13 +0700

    update firebase deploy

--- src/app/config/firebaseConfig.ts
+++ src/app/config/firebaseConfig.ts
@@ -2,15 +2,17 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+import { appConfig } from './appConfig'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || appConfig.firebase?.apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || appConfig.firebase?.authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || appConfig.firebase?.projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || appConfig.firebase?.storageBucket,
+  messagingSenderId:
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || appConfig.firebase?.messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || appConfig.firebase?.appId,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || appConfig.firebase?.measurementId,
 }
 
 // Initialize Firebase
